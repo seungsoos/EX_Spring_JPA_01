38a87a0d1098ba48b7ed8148e0c9eaa6d4987688
seungsoo <seung_soos@naver.com>
2023-11-26 23:40:45 +0900

    - [section 4] 회원 도메인 개발

---
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,51 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    public void 회원가입() {
+        //given
+        Member member = new Member();
+        member.setName("memberA");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        assertEquals(member, memberRepository.findOne(saveId));
+    }
+
+    @Test
+    public void 중복예외() {
+        //given
+        Member memberA = new Member();
+        memberA.setName("memberA");
+        Member memberB = new Member();
+        memberB.setName("memberA");
+
+        //when
+        memberService.join(memberA);
+
+        //then
+        assertThrows(IllegalStateException.class, () -> memberService.join(memberB));
+    }
+}
