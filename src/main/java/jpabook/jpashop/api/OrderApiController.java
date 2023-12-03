1b2765d148e76540f426f457ad187246c098c2d8
seungsoo <seung_soos@naver.com>
2023-12-03 13:26:50 +0900

    - Batch Size 적용

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -4,7 +4,9 @@ import jpabook.jpashop.domain.*;
 import jpabook.jpashop.repository.OrderRepository;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -17,9 +19,12 @@ import java.util.stream.Collectors;
  * 2. Entity를 직접 반환 하지 말고, DTO로 반환하여 반환
  * 3. Fetch Join시 ToMany의 경우 페이징 불가 (데이터가 많을경우 out of memory)
  *     => firstResult/maxResults specified with collection fetch; applying in memory
+ *
+ * Batch Size 적용
  */
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class OrderApiController {
 
     private final OrderRepository orderRepository;
@@ -41,25 +46,39 @@ public class OrderApiController {
     @GetMapping("/api/v2/orders")
     public List<OrderDto> ordersV2() {
         List<Order> orders = orderRepository.findAllByString(new OrderSearch());
-        System.out.println(" ================================= ");
         List<OrderDto> collect = orders.stream().map(order -> new OrderDto(order))
                 .collect(Collectors.toList());
-        System.out.println(" ================================= ");
         return collect;
     }
 
     @GetMapping("/api/v3/orders")
     public List<OrderDto> ordersV3() {
         List<Order> list =orderRepository.findAllWithItem();
-        System.out.println(" ================================= ");
         List<OrderDto> collect = list.stream().map(order -> new OrderDto(order))
                 .collect(Collectors.toList());
-
-        System.out.println(" ================================= ");
         return collect;
     }
 
+    @GetMapping("/api/v3.1/orders")
+    public List<OrderDto> ordersV3_page() {
+        List<Order> list =orderRepository.findAllWithMemberDelivery();
+        List<OrderDto> collect = list.stream().map(order -> new OrderDto(order))
+                .collect(Collectors.toList());
 
+        return collect;
+    }
+    @GetMapping("/api/v3.1/page/orders")
+    public List<OrderDto> ordersV3_page(
+            @RequestParam(value = "offset",defaultValue = "0") int offset,
+            @RequestParam(value = "limit",defaultValue = "100") int limit
+                                        ) {
+        // ToOne 관계는 페이징 처리가 가능하다.
+        List<Order> list =orderRepository.findAllWithMemberDelivery(offset,limit);
+        List<OrderDto> collect = list.stream().map(order -> new OrderDto(order))
+                .collect(Collectors.toList());
+
+        return collect;
+    }
 
     @Data
     static class OrderDto {
@@ -72,7 +91,7 @@ public class OrderApiController {
         private List<OrderItemDto> orderItems;
 
         public OrderDto(Order order) {
-            System.out.println(" 생성자 시작================================= ");
+            log.info("order 생성자 시작 ={}",order);
             orderId = order.getId();
             name = order.getMember().getName();
             orderDate = order.getOrderDate();
@@ -81,7 +100,7 @@ public class OrderApiController {
             orderItems = order.getOrderItems().stream()
                         .map(orderItem -> new OrderItemDto(orderItem))
                         .collect(Collectors.toList());
-            System.out.println(" 생성자 종료================================= ");
+            log.info("order 생성자 끝 ={}",order);
         }
     }
 
